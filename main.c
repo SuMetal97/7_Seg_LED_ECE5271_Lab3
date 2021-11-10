a54d115e22524fc1c762a321f91e9c468ea13a45
Dakota Wrigley <43604399+SuMetal97@users.noreply.github.com>
2021-11-10 17:40:03 -0500

    Update main.c

--- main.c
+++ main.c
@@ -2,122 +2,199 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define LED1_SHIFT (1) //yellow LED portA
-#define LED2_SHIFT (2)	//green LED portA
-#define LED3_SHIFT (4)	//blue LED portD
-#define LED4_SHIFT (12) //red LED portA
-#define SW1_SHIFT (5)		//ETS switch portA
-#define SW2_SHIFT (13)  //FLS switch portA
-#define SW3_SHIFT (16) //overflow switch portA
+//Define the ports for each output to the 7 Seg LED (Port A)
+#define Seg_A (1)
+#define Seg_B (2)
+#define Seg_C (12)
+#define Seg_D (4)
+#define Seg_E (5)
+#define Seg_F (13)
+#define Seg_G (16)
+
+/*Define enable, is not needed for control, but is to turn the 7 Seg on
+This can have a switch in to control on/off features but is not needed 
+for this example*/ 
+
+#define Enable (17) //Port A
+
+//Define the Switches that will be controling the 7 Seg output (Port D)
+#define SW_1 (5)
+#define SW_2 (0)
+#define SW_3 (2)
+#define SW_4 (3)
 
 #define MASK(x) (1UL << (x))
-void Basic_Light_Switching_Example(void);
+void Initialize(void);
+int check_switches(void);
 
 
-
-void Basic_Light_Switching_Example(void){
-	SIM->SCGC5 |= SIM_SCGC5_PORTA_MASK;
-	SIM->SCGC5 |= SIM_SCGC5_PORTD_MASK;
-	PORTA->PCR[LED1_SHIFT]  &= ~PORT_PCR_MUX_MASK;
-	PORTA->PCR[LED1_SHIFT] |= PORT_PCR_MUX(1);
-	PORTA->PCR[LED2_SHIFT]  &= ~PORT_PCR_MUX_MASK;
-	PORTA->PCR[LED2_SHIFT] |= PORT_PCR_MUX(1);
-	PORTD->PCR[LED3_SHIFT]  &= ~PORT_PCR_MUX_MASK;
-	PORTD->PCR[LED3_SHIFT] |= PORT_PCR_MUX(1);
-	PORTA->PCR[LED4_SHIFT]  &= ~PORT_PCR_MUX_MASK;
-	PORTA->PCR[LED4_SHIFT] |= PORT_PCR_MUX(1);
-	PORTA->PCR[SW1_SHIFT]  &= ~PORT_PCR_MUX_MASK;
-	PORTA->PCR[SW1_SHIFT] |= PORT_PCR_MUX(1);
-	PORTA->PCR[SW2_SHIFT]  &= ~PORT_PCR_MUX_MASK;
-	PORTA->PCR[SW2_SHIFT] |= PORT_PCR_MUX(1);
-	PORTA->PCR[SW3_SHIFT]  &= ~PORT_PCR_MUX_MASK;
-	PORTA->PCR[SW3_SHIFT] |= PORT_PCR_MUX(1);
-	
-	PTA->PDDR |= MASK(LED1_SHIFT) | MASK(LED2_SHIFT) | MASK(LED4_SHIFT);
-	PTD->PDDR |= MASK(LED3_SHIFT);
-	PTA->PDDR &= ~MASK(SW1_SHIFT);
-	PTA->PDDR &= ~MASK(SW2_SHIFT);
-	PTA->PDDR &= ~MASK(SW3_SHIFT);
-	
-	//PTA->PSOR = MASK(LED1_SHIFT);
-	//PTA->PCOR = MASK(LED2_SHIFT);
-
-}
-int main(void){
-	char location = 'a';
-	Basic_Light_Switching_Example();
-	
-	while(1){
-		switch(location){
-			case 'E':
-				PTA->PCOR = MASK(LED1_SHIFT); //yellow led off
-				PTA->PCOR = MASK(LED2_SHIFT); // green led off
-				PTD->PSOR = MASK(LED3_SHIFT); // blue led on
-				PTA->PCOR = MASK(LED4_SHIFT); // red LED off
-				//if ETS Switch is not pressed, go to Water in tank, else stay at empty
-				if(PTA->PDIR &MASK(SW1_SHIFT)){
-					location = 'W';
-				} else{
-				}
-				break;
-				
-			case 'W':
-				PTA->PCOR = MASK(LED1_SHIFT); //yellow led off
-				PTA->PSOR = MASK(LED2_SHIFT); //green led on
-				PTD->PSOR = MASK(LED3_SHIFT); //blue led on
-				PTA->PCOR = MASK(LED4_SHIFT); // red LED off
-				//if ETS switch is pressed, go back to tank empty
-				if(PTA->PDIR &MASK(SW1_SHIFT)){
-				} else{
-					location = 'E';
-				}
-				//if FLS switch is pressed, go to tank full
-				if(PTA->PDIR &MASK(SW2_SHIFT)){
-				} else{
-					location = 'F';
-				}
-				break;
-			
-			case 'F':
-				PTA->PSOR = MASK(LED1_SHIFT);//yellow led on
-				PTA->PCOR = MASK(LED2_SHIFT);//green led off
-				PTD->PSOR = MASK(LED3_SHIFT);//blue led on
-				PTA->PCOR = MASK(LED4_SHIFT); // red LED off
-				//if FLS switch is not pressed, go back to water in tank
-				if(PTA->PDIR &MASK(SW2_SHIFT)){
-					location = 'W';
-				} else{
-				}
-				//if overflow switch is pressed, go to overflow
-				if(PTA->PDIR &MASK(SW3_SHIFT)){
-				} else{
-					location = 'O';
-				}
-				break;
-				
-			case 'O':
-				PTA->PCOR = MASK(LED1_SHIFT);//yellow led off
-				PTA->PCOR = MASK(LED2_SHIFT);//green led off
-				PTD->PCOR = MASK(LED3_SHIFT);//blue led off
-				PTA->PSOR = MASK(LED4_SHIFT); // red LED on
-				//if overflow switch is not pressed, go back to tank full
-				if(PTA->PDIR & MASK(SW3_SHIFT)){
-					location = 'F';
-				} else{
-				}
-				break;
-			
-			default:
-				PTA->PCOR = MASK(LED1_SHIFT);//yellow led off
-				PTA->PCOR = MASK(LED2_SHIFT);//green led off
-				PTD->PCOR = MASK(LED3_SHIFT);//blue led off
-				PTA->PCOR = MASK(LED4_SHIFT); // red LED off
-				//when ETS is pressed, go to tank empty
-				if(PTA->PDIR & MASK(SW1_SHIFT)){
-				} else{
-					location = 'E';
-				}	
-				break;
-		}	
+int check_switches(void){
+	int Switch_Total = 0;
+	if(PTA->PDIR & MASK(SW_1)){
+	}else{
+		Switch_Total +=1;
 	}
+	if(PTA->PDIR & MASK(SW_2)){
+	}else{
+		Switch_Total +=2;
+	}
+	if(PTA->PDIR & MASK(SW_3)){
+	}else{
+		Switch_Total +=4;
+	}
+	if(PTA->PDIR & MASK(SW_4)){
+	}else{
+		Switch_Total +=8;
+	}
+	return Switch_Total;
+}
+
+void Initialize(void){
+	SIM_SCGC5 |= SIM_SCGC5_PORTA_MASK;
+	SIM->SCGC5 |= SIM_SCGC5_PORTD_MASK;
+	PORTA->PCR[Seg_A]  &= ~PORT_PCR_MUX_MASK;
+	PORTA->PCR[Seg_A] |= PORT_PCR_MUX(1);
+	PORTA->PCR[Seg_B]  &= ~PORT_PCR_MUX_MASK;
+	PORTA->PCR[Seg_B] |= PORT_PCR_MUX(1);
+	PORTA->PCR[Seg_C]  &= ~PORT_PCR_MUX_MASK;
+	PORTA->PCR[Seg_C] |= PORT_PCR_MUX(1);
+	PORTA->PCR[Seg_D]  &= ~PORT_PCR_MUX_MASK;
+	PORTA->PCR[Seg_D] |= PORT_PCR_MUX(1);
+	PORTA->PCR[Seg_E]  &= ~PORT_PCR_MUX_MASK;
+	PORTA->PCR[Seg_E] |= PORT_PCR_MUX(1);
+	PORTA->PCR[Seg_F]  &= ~PORT_PCR_MUX_MASK;
+	PORTA->PCR[Seg_F] |= PORT_PCR_MUX(1);
+	PORTA->PCR[Seg_G]  &= ~PORT_PCR_MUX_MASK;
+	PORTA->PCR[Seg_G] |= PORT_PCR_MUX(1);
+	PORTA->PCR[Enable]  &= ~PORT_PCR_MUX_MASK;
+	PORTA->PCR[Enable] |= PORT_PCR_MUX(1);
+
+	PORTD->PCR[SW_1]  &= ~PORT_PCR_MUX_MASK;
+	PORTD->PCR[SW_1] |= PORT_PCR_MUX(1);
+	PORTD->PCR[SW_2]  &= ~PORT_PCR_MUX_MASK;
+	PORTD->PCR[SW_2] |= PORT_PCR_MUX(1);
+	PORTD->PCR[SW_3]  &= ~PORT_PCR_MUX_MASK;
+	PORTD->PCR[SW_3] |= PORT_PCR_MUX(1);
+	PORTD->PCR[SW_4]  &= ~PORT_PCR_MUX_MASK;
+	PORTD->PCR[SW_4] |= PORT_PCR_MUX(1);
+
+	PTA->PDDR |= MASK(Seg_A) | MASK(Seg_B) | MASK(Seg_C) | MASK(Seg_D) | MASK(Seg_E) | MASK(Seg_F) | MASK(Seg_G) | MASK(Enable);
+	PTD->PDDR &= ~MASK(SW_1);
+	PTD->PDDR &= ~MASK(SW_2);
+	PTD->PDDR &= ~MASK(SW_3);
+	PTD->PDDR &= ~MASK(SW_4);
+}
+
+int main(void){
+	int Switch_count;
+	while (1){
+		Switch_count = 0;
+		Switch_count = check_switches();
+		switch (Switch_count){
+			case 0:
+				PTA->PSOR = MASK(Seg_A);
+				PTA->PSOR = MASK(Seg_B);
+				PTA->PSOR = MASK(Seg_C);
+				PTA->PSOR = MASK(Seg_D);
+				PTA->PSOR = MASK(Seg_E);
+				PTA->PSOR = MASK(Seg_F);
+				PTA->PCOR = MASK(Seg_G);
+				break;
+			case 1:
+				PTA->PCOR = MASK(Seg_A);
+				PTA->PSOR = MASK(Seg_B);
+				PTA->PSOR = MASK(Seg_C);
+				PTA->PCOR = MASK(Seg_D);
+				PTA->PCOR = MASK(Seg_E);
+				PTA->PCOR = MASK(Seg_F);
+				PTA->PCOR = MASK(Seg_G);
+				break;
+			case 2:
+				PTA->PSOR = MASK(Seg_A);
+				PTA->PSOR = MASK(Seg_B);
+				PTA->PCOR = MASK(Seg_C);
+				PTA->PSOR = MASK(Seg_D);
+				PTA->PSOR = MASK(Seg_E);
+				PTA->PCOR = MASK(Seg_F);
+				PTA->PSOR = MASK(Seg_G);
+				break;
+			case 3:
+				PTA->PSOR = MASK(Seg_A);
+				PTA->PSOR = MASK(Seg_B);
+				PTA->PSOR = MASK(Seg_C);
+				PTA->PSOR = MASK(Seg_D);
+				PTA->PCOR = MASK(Seg_E);
+				PTA->PCOR = MASK(Seg_F);
+				PTA->PSOR = MASK(Seg_G);
+				break;
+			case 4:
+				PTA->PCOR = MASK(Seg_A);
+				PTA->PSOR = MASK(Seg_B);
+				PTA->PSOR = MASK(Seg_C);
+				PTA->PCOR = MASK(Seg_D);
+				PTA->PCOR = MASK(Seg_E);
+				PTA->PSOR = MASK(Seg_F);
+				PTA->PSOR = MASK(Seg_G);
+				break;
+			case 5:
+				PTA->PSOR = MASK(Seg_A);
+				PTA->PCOR = MASK(Seg_B);
+				PTA->PSOR = MASK(Seg_C);
+				PTA->PSOR = MASK(Seg_D);
+				PTA->PCOR = MASK(Seg_E);
+				PTA->PSOR = MASK(Seg_F);
+				PTA->PSOR = MASK(Seg_G);
+				break;
+			case 6:
+				PTA->PSOR = MASK(Seg_A);
+				PTA->PCOR = MASK(Seg_B);
+				PTA->PSOR = MASK(Seg_C);
+				PTA->PSOR = MASK(Seg_D);
+				PTA->PSOR = MASK(Seg_E);
+				PTA->PSOR = MASK(Seg_F);
+				PTA->PCOR = MASK(Seg_G);
+				break;
+			case 7:
+				PTA->PSOR = MASK(Seg_A);
+				PTA->PSOR = MASK(Seg_B);
+				PTA->PSOR = MASK(Seg_C);
+				PTA->PCOR = MASK(Seg_D);
+				PTA->PCOR = MASK(Seg_E);
+				PTA->PCOR = MASK(Seg_F);
+				PTA->PCOR = MASK(Seg_G);
+				break;
+			case 8:
+				PTA->PSOR = MASK(Seg_A);
+				PTA->PSOR = MASK(Seg_B);
+				PTA->PSOR = MASK(Seg_C);
+				PTA->PSOR = MASK(Seg_D);
+				PTA->PSOR = MASK(Seg_E);
+				PTA->PSOR = MASK(Seg_F);
+				PTA->PSOR = MASK(Seg_G);
+				break;
+			case 9:
+				PTA->PSOR = MASK(Seg_A);
+				PTA->PSOR = MASK(Seg_B);
+				PTA->PSOR = MASK(Seg_C);
+				PTA->PSOR = MASK(Seg_D);
+				PTA->PCOR = MASK(Seg_E);
+				PTA->PSOR = MASK(Seg_F);
+				PTA->PSOR = MASK(Seg_G);
+				break;
+				
+			default:
+				PTA->PCOR = MASK(Seg_A);
+				PTA->PCOR = MASK(Seg_B);
+				PTA->PCOR = MASK(Seg_C);
+				PTA->PCOR = MASK(Seg_D);
+				PTA->PCOR = MASK(Seg_E);
+				PTA->PCOR = MASK(Seg_F);
+				PTA->PSOR = MASK(Seg_G);
+		
+		}
+	
+	}
+
+
+
 }
